bce09cab56f73d7415677fe6a4ce0bf92ad4bb47
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 15:51:58 +0100

    creatingmessage

--- Api/controllers/message.js
+++ Api/controllers/message.js
@@ -1,3 +1,18 @@
 const conn = require('../mysqlConfig')
 const jwt = require('jsonwebtoken')
 const config = require('../config/jwt.secret')
+
+
+exports.createMessage = (req, res, next) => {
+  const message = req.body
+  conn.query('INSERT INTO messages SET ?', message, function (
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      return res.status(400).json(error)
+    }
+    return res.status(201).json({ message: 'Votre message a bien été posté !' })
+  })
+}
