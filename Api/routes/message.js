5550ff561a2d077cdddcefb22c3de1cc8b8745fd
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 15:56:35 +0100

    links

--- Api/routes/message.js
+++ Api/routes/message.js
@@ -1,4 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-
+const messageCtrl = require('../controllers/message')
+const auth = require('../middelware/auth')
+const multer = require('../middelware/multer-config')
