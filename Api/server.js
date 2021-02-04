e8d5b7bf53f8f301a7d0f8dda8a8992ffc602e88
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 16:00:29 +0100

    normalizePort

--- Api/server.js
+++ Api/server.js
@@ -1,2 +1,14 @@
 const http = require('http')
 const app = require('./app')
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10)
+
+  if (isNaN(port)) {
+    return val
+  }
+  if (port >= 0) {
+    return port
+  }
+  return false
+}
