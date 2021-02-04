7d22841f71ddbc247e952b04f25ebf45f12ca25d
avehghasemian <av.ghasemian@gmail.com>
2021-02-04 16:09:37 +0100

    callback

--- Api/middelware/multer-config.js
+++ Api/middelware/multer-config.js
@@ -9,4 +9,13 @@ const MIME_TYPES = {
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, 'images')
+  },
+  filename: (req, file, callback) => {
+    // On va renommer le fichier
+    const name = file.originalname.split(' ').join('_') // Pour éliminer les éventuelles 'white space' du nom d'origine
+    const extension = MIME_TYPES[file.mimetype]
+    callback(null, name + Date.now() + '.' + extension) // création du nom final
   }
+})
+
+module.exports = multer({ storage }).single('image')
