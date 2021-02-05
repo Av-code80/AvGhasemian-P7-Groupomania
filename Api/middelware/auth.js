e8651ce607ebb55cc3ecbc4f38855871d34db6ce
avehghasemian <av.ghasemian@gmail.com>
2021-02-05 17:41:03 +0100

    autheErrorfixing

--- Api/middelware/auth.js
+++ Api/middelware/auth.js
@@ -1,5 +1,4 @@
 // Middleware qui vient vérifier le TOKEN à chaque emprunt d'une route
-
 const jwt = require('jsonwebtoken')
 const config = require('../config/jwt.secret') // On récupère la clé pour le TOKEN
 
